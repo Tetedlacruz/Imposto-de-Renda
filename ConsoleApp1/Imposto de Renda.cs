7f8324a4c0c79335354e3544b40e423190a57d5e
2022101197 <2022101197@LAB2-010>
2023-08-29 19:14:45 -0300

    Adicionar arquivos de projeto.

---
+++ ConsoleApp1/Imposto de Renda.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace ImpostodeRenda
+{
+    public class Imposto_de_Renda
+    {
+        public string CalcularImposto(double salario)
+        {
+            string calculo;
+            double totalimposto;
+            double deducao;
+            if (salario <= 1903.98)
+            {
+                string insento = "Você está insento";
+                return insento;
+            }
+            else if (salario > 1903.98 && salario <= 2826.65)
+            {
+                deducao = 142.80;
+                totalimposto = (salario / 100 * 7.5) - deducao;
+                calculo = $"Será deduzido o valor de {Math.Round(totalimposto, 2)}";
+                return calculo;
+            }
+            else if (salario > 2826.66 && salario <= 3751.05)
+            {
+                deducao = 354.80;
+                totalimposto = (salario / 100 * 15) - deducao;
+                calculo = $"Será deduzido o valor de {Math.Round(totalimposto, 2)}";
+                return calculo;
+            }
+            else if (salario > 3751.06 && salario <= 4664.68)
+            {
+                deducao = 636.13;
+                totalimposto = (salario / 100 * 22.5) - deducao;
+                calculo = $"Será deduzido o valor de {Math.Round(totalimposto, 2)}";
+                return calculo;
+            }
+            else
+            {
+                deducao = 869.36;
+                totalimposto = (salario / 100 * 27.5) - deducao;
+                calculo = $"Será deduzido o valor de {Math.Round(totalimposto, 2)}";
+                return calculo;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,6 @@
+﻿using System;
+using ImpostodeRenda;
+
+Imposto_de_Renda calcular = new Imposto_de_Renda();
+double valor = 2000;
+Console.WriteLine(calcular.CalcularImposto(valor));
